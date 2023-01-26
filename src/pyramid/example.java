37cc77bfce1692fe30d92b5373e6bb316fa56fd1
Avigneshsumathi <vigneshjollyman@gmail.com>
2023-01-26 12:29:42 +0530

    first commit

---
+++ src/pyramid/example.java
@@ -0,0 +1,20 @@
+package pyramid;
+import java.util.Scanner;
+public class example {
+	public static void main (String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("   Enter a mark   ");
+		int mark = scanner.NextInt();
+		scanner.nextLine();
+		System.out.println(Enter a mark + Enter a mark + enter a mark  );
+		int mark1 = scanner.nextInt();
+		scanner.nextLine();
+		System.out.println(Enter a mark + enter a mark + enter a mark + enter a mark + enter a mark);
+		int mark2 = scanner.nextInt();
+		scanner.nextLine();
+		System.out.println(Enter a mark + enter a mark + enter a mark + enter a mark + enter a mark + enter a mark + enter a mark);
+		int mark3 = scanner.nextInt();
+		
+	}
+
+}
